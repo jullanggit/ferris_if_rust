58fb3ad59675af0c14653f2841694b9654e8e38a
Jul Lang <jullanggit@proton.me>
2024-12-25 00:04:59 +0100

    Fix checking for parents of the wrong path. Also, make the current dir slice shadow the owned one, to avoid issues like this

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,17 @@ use std::env;
 
 fn main() {
     let current_dir = env::current_dir().expect("Failed to get current directory");
-    let mut current_dir_slice = current_dir.as_path();
+    let mut current_dir = current_dir.as_path();
 
     loop {
-        if current_dir_slice.join("Cargo.toml").is_file() {
+        dbg!(current_dir);
+        if current_dir.join("Cargo.toml").is_file() {
             print!("🦀 ");
             return;
         }
 
         if let Some(parent) = current_dir.parent() {
-            current_dir_slice = parent;
+            current_dir = parent;
         } else {
             break;
         }
